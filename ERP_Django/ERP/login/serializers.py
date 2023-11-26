dbbcaa69fe3e73aac93292af9ac1fc0cb8ddc451
rahuljoshi1729 <rahul22100@akgec.ac.in>
2023-11-26 20:50:33 +0530

    added(baceknd_files)

---
+++ ERP_Django/ERP/login/serializers.py
@@ -0,0 +1,76 @@
+
+from rest_framework import serializers
+from  .models import *
+
+class LoginSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    password = serializers.CharField(write_only=True)
+
+
+class UserSerialiazer(serializers.ModelSerializer):
+    class Meta:
+        model=LoginUser
+        fields=['user_id','password','is_verified']
+
+class dataeditorserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Student
+        fields=['user_id','first_name','last_name','admission_date','email','aadhar','phone_number','role','Branch','Year','semester','section','password','religion','dob']        
+
+    
+class VerifyOTPSerializer(serializers.Serializer):
+    otp = serializers.IntegerField()
+    email = serializers.EmailField()
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()    
+
+
+class PasswordtakingSerializer(serializers.Serializer):
+    password=serializers.CharField(write_only=True)
+    confirm_password=serializers.CharField(write_only=True)
+
+class subjecteditorserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Subjects
+        fields=['name','code','semester']
+
+
+class attendenceeditorserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Attendance
+        fields=['date','student_id','subject','is_present']
+
+class facultyeditorserializer(serializers.ModelSerializer):
+    class Meta:
+        model=Faculty
+        fields=['user_id','first_name','last_name','email','phone_number','role','Post','Department','password','aadhar','address','gender']   
+
+class classassignserializer(serializers.ModelSerializer):
+    class Meta:
+        model=classassigned
+        fields=['subject_code','faculty','semester','class_assigned']        
+
+class exameditorserializer(serializers.ModelSerializer):
+    class Meta:
+        model=exam
+        fields=['exam_name','total_marks','date','duration','session']
+
+
+class ExamDataAdmitResultserializer(serializers.ModelSerializer):
+    class Meta:
+        model=ExamDataAdmitResult
+        fields=['user_id','exam_name','session','result','admit_card']
+        
+        
+#serializer for giving exam data     
+
+class givingexamdataserializer(serializers.ModelSerializer):
+    class Meta:
+        model = exam
+        fields = '__all__'   
+        
+class givingexamadmitresultdataserializer(serializers.ModelSerializer):
+    class Meta:
+        model = exam
+        fields = '__all__'   
---
+++ ERP_Django/ERP/login/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import *
+
+urlpatterns = [
+    path('register/',register.as_view(),name='register'),
+    path('studentdataeditor/',studentdataeditor.as_view(),name='dataeditor'),
+    path('facultyeditor/',facultyeditor.as_view(),name='faculty_data'),
+    path('subjecteditor/',subjecteditor.as_view(),name='dataeditor'),
+    path('attendanceeditor/',attendanceeditor.as_view(),name='dataeditor'),
+    path('exameditor/',examdataeditiorview.as_view(),name='exam_data'),
+    path('examdataadmitresult/',examdataadmitresulteditiorview.as_view(),name='exam_data'),
+    path('classassign/',classassignview.as_view(),name='classassign'),
+    path('examdata/',get_examdata,name='exam_data'),
+    path("verifyotp/",VerifyOTP.as_view(),name="verifyotp"),
+    path("passwordreset/",PasswordResetRequest.as_view(),name="passwordresetrequest"),
+    path("password/reset/<str:token>/",PasswordReset.as_view(),name="passwordreset"),
+    path("attendance/",Attendanceview,name="attendance"),]
+    
